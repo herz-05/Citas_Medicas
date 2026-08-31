913c60b30bedd033fd4ceb385efa4c1dc8c1d558
Luis-Aguilar200 <luiseaguilar.es@gmail.com>
2026-08-31 16:49:32 -0600

    aws

---
+++ Persistence/Migrations/20260831221645_ActualizarModelo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActualizarModelo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Consultorios",
+                columns: table => new
+                {
+                    IdConsultorio = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumeroConsultorio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Piso = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Ubicacion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Estado = table.Column<bool>(type: "bit", nullable: false),
+                    FechaRegistro = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consultorios", x => x.IdConsultorio);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HorariosMedicos",
+                columns: table => new
+                {
+                    IdHorario = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdMedico = table.Column<int>(type: "int", nullable: false),
+                    IdConsultorio = table.Column<int>(type: "int", nullable: false),
+                    DiaSemana = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HoraInicio = table.Column<TimeSpan>(type: "time", nullable: false),
+                    HoraFin = table.Column<TimeSpan>(type: "time", nullable: false),
+                    Estado = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HorariosMedicos", x => x.IdHorario);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consultorios");
+
+            migrationBuilder.DropTable(
+                name: "HorariosMedicos");
+        }
+    }
+}
