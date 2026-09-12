365c4e1d4a94f3b72a4224d143a7e2c8dd4bd991
Luis-Aguilar200 <luiseaguilar.es@gmail.com>
2026-09-12 09:23:18 -0600

    cambios genericos

---
+++ Persistence/Migrations/20260912151014_ActualizacionModelos.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActualizacionModelos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Citas",
+                columns: table => new
+                {
+                    IdCita = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdPaciente = table.Column<int>(type: "int", nullable: false),
+                    IdMedico = table.Column<int>(type: "int", nullable: false),
+                    IdConsultorio = table.Column<int>(type: "int", nullable: false),
+                    IdEstadoCita = table.Column<int>(type: "int", nullable: false),
+                    FechaCita = table.Column<DateOnly>(type: "date", nullable: false),
+                    HoraInicio = table.Column<TimeOnly>(type: "time", nullable: false),
+                    HoraFin = table.Column<TimeOnly>(type: "time", nullable: false),
+                    MotivoConsulta = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Observaciones = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaRegistro = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Citas", x => x.IdCita);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContactosEmergencias",
+                columns: table => new
+                {
+                    IdContacto = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdPaciente = table.Column<int>(type: "int", nullable: false),
+                    NombreCompleto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Parentesco = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactosEmergencias", x => x.IdContacto);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EstadosCitas",
+                columns: table => new
+                {
+                    IdEstadoCita = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreEstado = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EstadosCitas", x => x.IdEstadoCita);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Turnos",
+                columns: table => new
+                {
+                    IdTurno = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdHorario = table.Column<int>(type: "int", nullable: false),
+                    IdPaciente = table.Column<int>(type: "int", nullable: false),
+                    NumeroTurno = table.Column<int>(type: "int", nullable: false),
+                    FechaTurno = table.Column<DateOnly>(type: "date", nullable: false),
+                    Estado = table.Column<bool>(type: "bit", nullable: false),
+                    FechaRegistro = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Turnos", x => x.IdTurno);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Citas");
+
+            migrationBuilder.DropTable(
+                name: "ContactosEmergencias");
+
+            migrationBuilder.DropTable(
+                name: "EstadosCitas");
+
+            migrationBuilder.DropTable(
+                name: "Turnos");
+        }
+    }
+}
